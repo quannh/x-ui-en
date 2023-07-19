a5dd07e08045353741fd277e9fd8e102c7b0603e
quannh <quannh.vp@gmail.com>
2023-07-20 00:01:56 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -7,6 +7,9 @@ plain='\033[0m'
 
 cur_dir=$(pwd)
 
+arch=arm64
+release=debian
+
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
 
